15c43937c7745d9bc56a67d4c422820e080111c9
Ana Beatriz de Araujo Veras <20171TIINF0031@ifpi.int>
2018-12-06 17:43:31 -0300

    Projeto Java

---
+++ Controle De hóspedes Sistema/Conexao/src/main/java/HotelBKM/Conexao/App.java
@@ -0,0 +1,13 @@
+package HotelBKM.Conexao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
